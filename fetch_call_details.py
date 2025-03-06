cf400a870a88bb48e0f6f8dd5f77b6c79137a971
Kevin Djabaku Ocansey <kevinocansey11@gmail.com>
2025-03-06 12:24:33 +0000

    Code from Sam merged

--- fetch_call_details.py
+++ fetch_call_details.py
@@ -8,12 +8,9 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 
 def get_call_id(call_id):
-    
     url = f"https://api.bland.ai/v1/calls/{call_id}"
-
     headers = {"authorization": api_key}
-
     response = requests.request("GET", url, headers=headers)
-
     print(response.text)
+
 
